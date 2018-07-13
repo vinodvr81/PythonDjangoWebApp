aab25f9f5ebd76fe752b5ba5acf02970243eb0ec
Vinod Rangaswamy Vukkalam <vinod.vukkalam@gmail.com>
2018-07-13 22:50:35 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 #app.config['DEBUG']=True
-
+@app.route("/")
+def index():
+    return "Hello World of Flask Python!"
 @app.route("/<string:name>")
 
 def hello(name):
